a741a1892e7312f04a5dae641988b3ee4c10f9ae
Daniel David <daniel.david@students.ptechnyc.org>
2017-07-18 19:23:27 +0000

    7/18/2017

---
+++ Upcoming_Events/events.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $("#1").keypress(function() {
+            $("#1").css("color", "#6CEC14");
+    });
+    $("#2").keypress(function() {
+            $("#2").css("color", "#6CEC14");
+    });
+     $("#3").keypress(function() {
+            $("#3").css("color", "#6CEC14");
+    });
+     $("#4").keypress(function() {
+            $("#4").css("color", "#6CEC14");
+    });
+     $("#5").keypress(function() {
+            $("#5").css("color", "#6CEC14");
+    });
+     $("#6").keypress(function() {
+            $("#6").css("color", "#6CEC14");
+    });
+     $("#7").keypress(function() {
+            $("#7").css("color", "#6CEC14");
+    });
+     $("#8").keypress(function() {
+            $("#8").css("color", "#6CEC14");
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
